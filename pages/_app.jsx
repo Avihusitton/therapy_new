5cc4dc6f9f38a35f3992940a2052c8791591f0f0
Avihu Sitton <avihu.sitton@gmail.com>
2026-04-27 22:20:07 +0300

    revert: disable dark mode and fix css syntax

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,19 +3,15 @@ import '@/index.css';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClientInstance } from '@/lib/query-client';
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "next-themes";
-
 import { AudioProvider } from '@/contexts/AudioContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClientInstance}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
-        <AudioProvider>
-          <Component {...pageProps} />
-          <Toaster />
-        </AudioProvider>
-      </ThemeProvider>
+      <AudioProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </AudioProvider>
     </QueryClientProvider>
   );
 }
